0abf592cd8ce41e6989ba570c48e56f4b7782263
Rangel-Floripa <rangelflorianopolis@gmail.com>
2022-09-03 07:15:15 -0300

    Aula de 03/09/22

---
+++ app/main.js
@@ -0,0 +1,12 @@
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+getBuscarLivrosDaAPI();
+
+
+
+async function getBuscarLivrosDaAPI(){
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    console.table(livros);
+
+}
